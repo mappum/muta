ad09cabab65cb12b30a4e2080cd523a05faf8605
Matt Bell <mappum@gmail.com>
2018-08-28 14:16:06 -0700

    Added ArrayPatch tests

---
+++ test/arrayPatch.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const test = require('tape')
+const ArrayPatch = require('../src/arrayPatch.js')
+const {
+  PUSH,
+  POP,
+  UNSHIFT,
+  SHIFT
+} = ArrayPatch
+
+test('ArrayPatch', (t) => {
+  t.test('create instance', (t) => {
+    let state = {}
+    let patch = new ArrayPatch(state)
+    t.deepEquals(patch[PUSH], [])
+    t.deepEquals(patch[UNSHIFT], [])
+    t.equals(patch[POP], 0)
+    t.equals(patch[SHIFT], 0)
+    t.deepEquals(state, {})
+    t.end()
+  })
+
+  t.test('non-empty grow array', (t) => {
+    let state = {}
+    let patch = new ArrayPatch(state)
+    patch[PUSH].push(0)
+    t.deepEquals(patch[PUSH], [ 0 ])
+    t.deepEquals(state[PUSH], [ 0 ])
+    t.end()
+  })
+
+  t.test('empty grow array', (t) => {
+    let state = {}
+    let patch = new ArrayPatch(state)
+    patch[PUSH].push(0)
+    patch[PUSH].pop()
+    t.deepEquals(patch[PUSH], [])
+    t.false(PUSH in state)
+    t.end()
+  })
+
+  t.test('non-zero shrink amount', (t) => {
+    let state = {}
+    let patch = new ArrayPatch(state)
+    patch[POP] += 1
+    t.equals(patch[POP], 1)
+    t.equals(patch[SHIFT], 0)
+    t.equals(state[POP], 1)
+    t.end()
+  })
+
+  t.test('zero shrink amount', (t) => {
+    let state = {}
+    let patch = new ArrayPatch(state)
+    patch[POP] += 1
+    patch[POP] -= 1
+    t.equals(patch[POP], 0)
+    t.false(POP in state)
+    t.end()
+  })
+
+  t.test('get/set unknown keys', (t) => {
+    let state = {}
+    let patch = new ArrayPatch(state)
+    patch.foo = 123
+    t.equals(patch.foo, 123)
+    t.equals(state.foo, 123)
+    t.end()
+  })
+
+  t.test('get/set unset keys', (t) => {
+    let state = {}
+    let patch = new ArrayPatch(state)
+    t.equals(patch.foo, undefined)
+    t.equals(state.foo, undefined)
+    t.end()
+  })
+
+  t.end()
+})
