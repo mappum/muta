6fc408d8de27debd650f58de1734b8ade385a26d
Matt Bell <mappum@gmail.com>
2018-09-05 10:40:53 -0700

    Alias 'muta.getPatch' -> 'muta.patch', handle wrapping array targets, and export array symbols

--- index.js
+++ index.js
@@ -2,25 +2,40 @@
 
 const VirtualObject = require('./src/virtualObject.js')
 const { ASSIGN, DELETE } = VirtualObject
+const VirtualArray = require('./src/virtualArray.js')
+const { PUSH, POP, SHIFT, UNSHIFT } = VirtualArray
 
-module.exports = function muta (target) {
-  let patch = new VirtualObject(target)
+module.exports =
+function muta (target) {
+  let patch
+  if (Array.isArray(target)) {
+    patch = new VirtualArray(target)
+  } else {
+    patch = new VirtualObject(target)
+  }
   return patch.wrapper
 }
 
-module.exports.commit = function commit (wrapper) {
+module.exports.commit =
+function commit (wrapper) {
   let patch = unwrap(wrapper)
   patch.commit()
 }
 
-module.exports.patch = function patch (wrapper) {
+module.exports.patch =
+module.exports.getPatch =
+function patch (wrapper) {
   let patch = unwrap(wrapper)
   return patch.patch
 }
 
 Object.assign(module.exports, {
   ASSIGN,
-  DELETE
+  DELETE,
+  PUSH,
+  POP,
+  SHIFT,
+  UNSHIFT
 })
 
 function unwrap (wrapper) {
